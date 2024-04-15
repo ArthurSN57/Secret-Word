a6501eb036dfbe66b6c306b4886efddd9d2d23f6
ArthurSN57 <arthur.santos.nasc@outlook.com>
2024-04-14 22:33:49 -0300

    First Commit

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+	carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+	fruta: ["Banana", "Maçã", "Laranja", "Manga", "Abacaxi"],
+	corpo: ["Cabeça", "Ombro", "Joelho", "Pé", "Vesicula"],
+	computador: ["Memória", "Gabinete", "Processador", "Sistema", "Mouse"],
+	programação: ["Java", "Código", "Lógica", "React", "Objeto"],
+	alimento: ["Hamburguer", "Arroz", "Feijão", "Salada", "Churrasco"]
+};
